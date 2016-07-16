2968dabdb89743075125f859f5634e299edaa4f3
Lefel <lefel95@outlook.com>
2016-07-16 14:19:55 -0300

    adding all the files of the last commit

---
+++ produto.php
@@ -0,0 +1,8 @@
+
+    <?php
+        $cabecalho_title = "Produto da Mirror Fashion";
+        $cabecalho_css = '<link rel="stylesheet" href="css/produto.css">';
+        include("cabecalho.php"); 
+    ?>
+    <?php include("rodape.php"); ?>
+</body>
