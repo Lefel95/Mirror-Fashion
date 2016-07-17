a772398babdafda608021c6aa67823a691abbd48
Lefel <lefel95@outlook.com>
2016-07-17 03:25:21 -0300

    Added Checkout php and linked with produto.php by method POST

--- produto.php
+++ produto.php
@@ -9,7 +9,7 @@
             <div class="produto">
                 <h1>Fuzzy Cardigan</h1>
                 <p>por apenas R$129,00</p>
-                <form action="">
+                <form action="checkout.php" method="post">
                     <fieldset class="cores">
                         <legend>Escolha a cor:</legend>
                         <input type="radio" name="cor" value="verde" id="verde" checked>
@@ -24,6 +24,8 @@
                         <input type="range" min="36" max="46" value="42" step="2" name="tamanho" id="tamanho">
                     </fieldset>
                     <input type="submit" class="comprar" value="Comprar">
+                    <input type="hidden" name="nome" value="Fuzzy Cardigan">
+                    <input type="hidden" name="preco" value="129.00">
                 </form>
             </div>
             <div class="detalhes">
