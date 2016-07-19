c8c0e4a6acc963dffbbf7f2e0b19f25a7c2c4efd
Lefel <lefel95@outlook.com>
2016-07-19 16:52:26 -0300

    Added bootstrap to use in checkout.php

--- checkout.php
+++ checkout.php
@@ -4,23 +4,38 @@
     <meta charset="UTF-8">
     <title>Checkout Mirror Fashion</title>
     <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="css/bootstrap.css">
 </head>
 <body>
-    <h1>Ótima escolha!</h1>
-    <p> 
-        Obrigado por comprar na Mirror Fashion!
-        Preencha seus dados para efetivar a compra.
-    </p>
-    <h2>Sua Compra</h2>
-    <dl>
-        <dt>Cor</dt>
-        <dd><?= $_POST['cor'] ?><dd>
-        <dt>Tamanho</dt>
-        <dd><?= $_POST['tamanho'] ?></dd>
-        <dt>Produto</dt>
-        <dd><?= $_POST['nome'] ?></dd>
-        <dt>Preço</dt>
-        <dd><?= $_POST['preco'] ?></dd>
-    </dl>
-</body>
+   <div class="jumbotron">
+       <div class="container">
+            <h1>Ótima escolha!</h1>
+            <p> 
+                Obrigado por comprar na Mirror Fashion!
+                Preencha seus dados para efetivar a compra.
+            </p>
+       </div>
+   </div>
+   <div class="container">
+           <div class="panel panel-success">
+                <div class="panel-heading">
+                    <h2 class="panel-title">Sua Compra</h2>
+                </div>
+            <div class="panel-body">
+               <img src="img/produtos/foto<?= $_POST['id'] ?>-<?= $_POST['cor'] ?>.png" class="img-thumbnail img-responsive">
+                <dl>
+                    <dt>Cor</dt>
+                    <dd><?= $_POST['cor'] ?><dd>
+                    <dt>Tamanho</dt>
+                    <dd><?= $_POST['tamanho'] ?></dd>
+                    <dt>Produto</dt>
+                    <dd><?= $_POST['nome'] ?></dd>
+                    <dt>Preço</dt>
+                    <dd><?= $_POST['preco'] ?></dd>
+                </dl>
+            </div>
+        </div>
+   </div>
+   
+    </body>
 </html>
